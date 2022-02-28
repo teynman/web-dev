156c3f28806586874260caaa111125597097897d
teynman <t.wenyuanhuang@gmail.com>
2022-02-28 12:26:23 -0500

    a5 JavaScript sp22

--- public/tuiter/PostSummaryList/PostSummaryItem.js
+++ public/tuiter/PostSummaryList/PostSummaryItem.js
@@ -1,5 +1,16 @@
-const PostSummaryItem = (posts) => {
-    return (`
+const PostSummaryItem = (item) => {
+    return (`<li class="list-group-item bg">
+                        <div class="txt-info">
+                            <p class="topic">${item.topic}</p>
+                            <p class="userName">
+                                <span>${item.userName}</span>
+                                <i class="rad-fill"></i>
+                                <span class="time">- ${item.time}</span>
+                            </p>
+                            <p class="title">${item.title}</p>
+                        </div>
+                        <img src="${item.image}" />
+                    </li>
     `);
 }
 
--- public/tuiter/WhoToFollowList/who.js
+++ public/tuiter/WhoToFollowList/who.js
@@ -1,13 +1,23 @@
 export default [
-    {   avatarIcon: '../../images/java.png',
-        userName: 'Java', handle: 'Java', },
-    {   avatarIcon: '../../images/relativity.jpeg',
+    {
+        avatarIcon: '../images/java.jpg',
+        userName: 'Java', handle: 'Java',
+    },
+    {
+        avatarIcon: '../images/relativity.jpg',
         userName: 'Relativity Space',
-        handle: 'relativityspace', },
-    {   avatarIcon: '../../images/virgin.png',
+        handle: 'relativityspace',
+    },
+    {
+        avatarIcon: '../images/virgin.jpg',
         userName: 'Virgin Galactic',
-        handle: 'virgingalactic', },
-    {   avatarIcon: '../../images/nasa.png',
-        userName: 'NASA', handle: 'NASA', },
-    {   avatarIcon: '../../images/tesla.png',
-        userName: 'Tesla', handle: 'Tesla', }, ];
+        handle: 'virgingalactic',
+    },
+    {
+        avatarIcon: '../images/nasa.jpg',
+        userName: 'NASA', handle: 'NASA',
+    },
+    {
+        avatarIcon: '../images/tesla.jpg',
+        userName: 'Tesla', handle: 'Tesla',
+    },];
