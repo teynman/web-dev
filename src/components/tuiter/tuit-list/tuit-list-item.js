4217a3337a9b62d934ae02158174e965e905539d
teynman <t.wenyuanhuang@gmail.com>
2022-04-06 23:39:53 -0400

    a8 Node sp22

--- src/components/tuiter/tuit-list/tuit-list-item.js
+++ src/components/tuiter/tuit-list/tuit-list-item.js
@@ -1,6 +1,7 @@
 import React from "react";
 const TuitListItem = (props) => {
   const { tuit } = props;
+  console.log("tuit",tuit)
   return (
     <div style={{display:"flex",borderBottom:"1px solid #000",padding:"10px 0px"}}>
       <img alt="" style={{width:40,height:40,borderRadius:40,objectFit:"cover",backgroundColor:"#fff"}} src={tuit['logo-image']}></img>
@@ -14,16 +15,16 @@ const TuitListItem = (props) => {
         <div style={{display:"flex",marginTop:10}}>
           <div style={{display:"flex",alignItems:"center",marginRight:60}}>
             <img alt="" style={{width:20,height:20}} src={require("../images/comment.png")}></img>
-            <div style={{color:"#fff",marginLeft:5}}>{tuit.stats.retuits}</div>
-            <div style={{color:"#fff"}}>{tuit.stats.comments}</div>
+            <div style={{color:"#fff",marginLeft:5}}>{tuit.stats && tuit.stats.retuits}</div>
+            <div style={{color:"#fff"}}>{tuit.stats && tuit.stats.comments}</div>
           </div>
           <div style={{display:"flex",alignItems:"center",marginRight:60}}>
             <img alt="" style={{width:20,height:20}} src={require("../images/retweet.png")}></img>
-            <div style={{color:"#fff",marginLeft:5}}>{tuit.stats.retuits}</div>
+            <div style={{color:"#fff",marginLeft:5}}>{tuit.stats &&tuit.stats.retuits}</div>
           </div>
           <div style={{display:"flex",alignItems:"center",marginRight:60}}>
             <img alt="" style={{width:20,height:20}} src={require("../images/like.png")}></img>
-            <div style={{color:"#fff",marginLeft:5}}>{tuit.stats.likes}</div>
+            <div style={{color:"#fff",marginLeft:5}}>{tuit.stats &&tuit.stats.likes}</div>
           </div>
          
           <div style={{display:"flex",alignItems:"center",marginRight:60}}>
